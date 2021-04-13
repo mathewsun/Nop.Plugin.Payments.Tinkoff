c2ed98232609da1a88726333abd6377ac266305d
Alex <alex.pigalyov@gmail.com>
2021-04-14 00:36:34 +0300

    add TinkoffPaymentProcessor

---
+++ TinkoffPaymentProcessor.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using Nop.Core;
+using Nop.Core.Domain.Orders;
+using Nop.Services.Payments;
+using Nop.Services.Plugins;
+
+using System;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.Payments.Tinkoff
+{
+    public class TinkoffPAymentProcessor : BasePlugin, IPaymentMethod
+    {
+        private readonly IWebHelper _webHelper;
+
+        public TinkoffPAymentProcessor(
+            IWebHelper webHelper)
+        {
+            _webHelper = webHelper;
+        }
+
+        public override string GetConfigurationPageUrl()
+        {
+            return $"{_webHelper.GetStoreLocation()}Admin/PaymentTinkoff/Configure";
+        }
+
+        //Copied from PayPalStandart plugin
+        public bool CanRePostProcessPayment(Order order)
+        {
+            if (order == null)
+                throw new ArgumentNullException(nameof(order));
+
+            //let's ensure that at least 5 seconds passed after order is placed
+            //P.S. there's no any particular reason for that. we just do it
+            if ((DateTime.UtcNow - order.CreatedOnUtc).TotalSeconds < 5)
+                return false;
+
+            return true;
+        }
+
+        public decimal GetAdditionalHandlingFee(IList<ShoppingCartItem> cart)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ProcessPaymentRequest GetPaymentInfo(IFormCollection form)
+        {
+            return new ProcessPaymentRequest();
+        }
+
+        public string GetPublicViewComponentName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HidePaymentMethod(IList<ShoppingCartItem> cart)
+        {
+            return false;
+        }
+
+        public void PostProcessPayment(PostProcessPaymentRequest postProcessPaymentRequest)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ProcessPaymentResult ProcessPayment(ProcessPaymentRequest processPaymentRequest)
+        {
+            return new ProcessPaymentResult();
+        }
+
+        public IList<string> ValidatePaymentForm(IFormCollection form)
+        {
+            return new List<string>();
+        }
+
+        public CapturePaymentResult Capture(CapturePaymentRequest capturePaymentRequest)
+        {
+            return new CapturePaymentResult { Errors = new[] { "Capture method not supported" } };
+        }
+
+        public RefundPaymentResult Refund(RefundPaymentRequest refundPaymentRequest)
+        {
+            return new RefundPaymentResult { Errors = new[] { "Refund method not supported" } };
+        }
+
+        public VoidPaymentResult Void(VoidPaymentRequest voidPaymentRequest)
+        {
+            return new VoidPaymentResult { Errors = new[] { "Void method not supported" } };
+        }
+
+        public ProcessPaymentResult ProcessRecurringPayment(ProcessPaymentRequest processPaymentRequest)
+        {
+            return new ProcessPaymentResult { Errors = new[] { "Recurring payment not supported" } };
+        }
+
+        public CancelRecurringPaymentResult CancelRecurringPayment(CancelRecurringPaymentRequest cancelPaymentRequest)
+        {
+            return new CancelRecurringPaymentResult { Errors = new[] { "Recurring payment not supported" } };
+        }
+
+        #region Properties
+
+        public bool SupportCapture => false;
+
+        public bool SupportPartiallyRefund => false;
+
+        public bool SupportRefund => false;
+
+        public bool SupportVoid => false;
+
+        public RecurringPaymentType RecurringPaymentType => RecurringPaymentType.NotSupported;
+
+        public PaymentMethodType PaymentMethodType => PaymentMethodType.Redirection;
+
+        public bool SkipPaymentInfo => true;
+
+        public string PaymentMethodDescription => "";
+
+        #endregion
+    }
+}
